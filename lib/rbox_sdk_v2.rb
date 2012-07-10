26386b8c6730d6b5ef3bc9867c5578e25e4be994
Scott Stout <j.scott.stout@gmail.com>
2012-07-09 18:37:52 -0700

    futzing around

--- lib/rbox_sdk_v2.rb
+++ lib/rbox_sdk_v2.rb
@@ -1,5 +1,64 @@
 require "rbox_sdk_v2/version"
+require "rbox_sdk_v2/config"
+
+require 'rubygems'
+require 'nokogiri'
+require 'open-uri'
+require 'mechanize'
+
+=begin
+
+API_KEY='SCRUBBED'
+USER='SCRUBBED'
+PASS='SCRUBBED'
+V1URL='https://www.box.com/api/1.0'
+V2URL='https://www.box.com/api/2.0'
+
+=end
 
 module RboxSdkV2
-  # Your code goes here...
+
+  class Client
+
+    def self.get_ticket
+      begin
+        doc = Nokogiri::XML(open("#{V1URL}/rest?action=get_ticket&api_key=#{API_KEY}"))
+        return doc.xpath('//response/ticket').inner_text
+      rescue => e
+        puts e.backtrace
+        exit
+      end
+    end
+
+    def self.auth_ticket(ticket)
+      begin
+        agent = Mechanize.new
+        page = agent.get("https://www.box.com/api/1.0//auth/#{ticket}")
+        boxnetform=page.form('login_form1')
+        boxnetform.login = "#{USER}"
+        boxnetform.password = "#{PASS}"
+        page2 = agent.submit(boxnetform, boxnetform.buttons.first)
+        form = page2.forms.first
+        form.login = "#{USER}"
+        form.password = "#{PASS}"
+        page3 = agent.submit(form, form.buttons.first)
+        sleep 2
+        return "done"
+      rescue => e
+       puts e.backtrace
+       exit
+      end
+    end
+
+    def self.get_auth_token(ticket)
+      begin
+        doc = Nokogiri::XML(open("https://www.box.com/api/1.0/rest?action=get_auth_token&api_key=#{API_KEY}&ticket=#{ticket}"))
+        return doc.xpath('//response/auth_token').inner_text
+      rescue => e
+        puts e.backtrace
+        exit
+      end
+    end
+
+  end
 end
--- spec/rbox_sdk_v2_spec.rb
+++ spec/rbox_sdk_v2_spec.rb
@@ -2,16 +2,38 @@ require 'spec_helper'
 
 describe RboxSdkV2 do
 
-  it "should work" do
-    lambda do
-      RboxSdkV2.get_authtoken()
+=begin
+
+    def self.get_ticket
+    def self.auth_ticket(ticket)
+    def self.get_auth_token(ticket)
+=end
+
+
+  it "should get ticket" do
+   lambda do
+      rbox_ticket = RboxSdkV2::Client.get_ticket()
     end.should_not raise_error
+  end
+  
+      puts @rbox_ticket
+  it "should authorize ticket request" do
+    lambda do
+      rbox_ticket = RboxSdkV2::Client.get_ticket()
+      rbox_auth_ticket = RboxSdkV2::Client.auth_ticket(rbox_ticket) 
+    end.should_not raise_error 
   end
 
   it "should return an auth token" do
-    auth_token = RboxSdkV2.get_authtoken()
-    auth_token.should_not be_nil
+    lambda do
+      rbox_ticket = RboxSdkV2::Client.get_ticket()
+      rbox_auth_ticket = RboxSdkV2::Client.auth_ticket(rbox_ticket)
+      auth_token = RboxSdkV2::Client.get_auth_token(rbox_ticket)
+      puts auth_token.class
+      puts auth_token.size
+      puts auth_token.inspect
+      auth_token.should_not be_nil
+    end.should_not raise_error
   end
-
 
 end
